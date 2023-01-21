f602602941c17838ee52f0d53013f216f5b301fa
root <root@ip-172-31-90-147.ec2.internal>
2023-01-21 10:05:03 +0000

    task8.sh modified

--- linux/task8.sh
+++ linux/task8.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p "Enter your password" password
 
-length=`echo $(#password)`
+length=`echo ${#password}`
 if [[ $length -lt 8 ]]
 then
         echo "Password must be of atleast 8 characters!"
@@ -11,7 +11,7 @@ echo "$password" > /dev/null
 
 echo $password | grep "[A-Z]" | grep "[a-z]" | grep "[0-9]" | grep "[!@#$%^&*()]"
 
-if [[ $? -ne 0]]
+if [ [ $? -ne 0] ] 
 then
         echo "Password must contain atleast 1upper case, lowercase, digits and special characters"
         exit 2
