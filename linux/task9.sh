0cc6fc90c076a5817492f23088a5dfbdda6bb1bd
root <root@ip-172-31-90-147.ec2.internal>
2023-01-21 10:14:21 +0000

    task9.sh modified

--- linux/task9.sh
+++ linux/task9.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 function1 ()
 {
 	echo "The function1calls"
@@ -20,9 +18,6 @@ function3 ()
 	echo "Your name is $name"
 }
  
-=======
-=======
->>>>>>> de0a52d4c809ebcb5de0c81dd4bc014edb35baf0
 . fun.sh
 
 function1
@@ -30,7 +25,3 @@ function2
 function3
 function2
 
-<<<<<<< HEAD
->>>>>>> de0a52d4c809ebcb5de0c81dd4bc014edb35baf0
-=======
->>>>>>> de0a52d4c809ebcb5de0c81dd4bc014edb35baf0
