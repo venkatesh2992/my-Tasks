4c18b3aeec90ec27b1e1a534ef120d5d24b27267
root <root@ip-172-31-90-147.ec2.internal>
2023-01-21 06:17:10 +0000

    Modified task4.sh

--- linux/task4.sh
+++ linux/task4.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
+echo "Please enter the directory path"
+read dir 
+dir=`pwd`
+find -name "*.java" -type f > java_files.txt
+for file in `ls -1 $dir/*.java`
+do
+	if [ -f $file ]
+	then
+		echo $file
+	fi
+done
 
+if [ $# -eq 0 ]
+then
+	echo "No files found in $dir"
+else
+	echo "Found $# files in $dir"
+fi
+if [ $# -gt 10 ]
+then
+	echo "Lasr 10 days only"
+else
+	echo "All files"
+fi
+
+rm -f $dir/*.java
 
