a4c666e4ffcd0976f2f97793b776926790921824
root <root@ip-172-31-88-5.ec2.internal>
2023-07-31 04:15:56 +0000

    task4 has been modifies

--- linux/task4.sh
+++ linux/task4.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
-echo "Please enter the directory path"
-read dir 
-dir=`pwd`
-find -name "*.java" -type f > java_files.txt
-for file in `ls -1 $dir/*.java`
-do
-	if [ -f $file ]
-	then
-		echo $file
-	fi
+
+find_and_delete_java_files() {
+    local directory="$1"
+
+    java_files=$(find "$directory" -type f -name "*.java")
+
+    file_count=$(echo "$java_files" | wc -l)
+
+    if [ $file_count -eq 0 ]; then
+        echo "No Java files found in directory '$directory'."
+        return
+    fi
+
+    echo "Found $file_count Java file(s) in directory '$directory'."
+
+    ten_days_ago=$(date -d "10 days ago" +%s)
+
+    for file in $java_files; do
+        last_modified=$(stat -c %Y "$file")
+
+        if [ $last_modified -ge $ten_days_ago ]; then
+            echo "Deleting file: $file"
+        fi
+    done
+}
+
+read -p "Enter the directory path(s) (separate multiple directories with spaces): " input_directories
+
+IFS=' ' read -r -a directories <<< "$input_directories"
+
+for dir in "${directories[@]}"; do
+    find_and_delete_java_files "$dir"
 done
-
-if [ $# -eq 0 ]
-then
-	echo "No files found in $dir"
-else
-	echo "Found $# files in $dir"
-fi
-if [ $# -gt 10 ]
-then
-	echo "Lasr 10 days only"
-else
-	echo "All files"
-fi
-
-rm -f $dir/*.java
 
