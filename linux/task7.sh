630cf3e3276b19d16f9b0dbdf2b74232f241c22f
root <root@ip-172-31-88-5.ec2.internal>
2023-07-31 05:27:55 +0000

    task7 has been modifies

--- linux/task7.sh
+++ linux/task7.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-echo "Please enter your username (must be uppercase and without spaces):"
-read username
+function is_uppercase() {
+  [[ $1 == [[:upper:]]* && $1 == "${1^^}" ]]
+}
 
-if [ [ $username =~ [A-Z]+  ] ] && [ [ ! $username =~ [ ]  ] ]
-then
-  echo "Username accepted"
-else
-  echo "Invalid username. Please enter a valid username (must be uppercase and without spaces)."
+
+while true; do
+  read -p "Please enter your username (uppercase letters only, no spaces): " username
+
+  if [[ $username == *" "* ]]; then
+    echo "Error: Username should not contain spaces. Please try again."
+  else
+	  
+    if [ -z "$username" ]; then
+      echo "Error: Username cannot be empty. Please try again."
+    else
+
+      if is_uppercase "$username"; then
+        echo "Username accepted: $username"
+        break
+      else
+        echo "Error: Username should be in uppercase letters only. Please try again."
+      fi
+    fi
+  fi
+done
+
